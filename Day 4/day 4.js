88650a1c23046fbe8c6239367b1306a5f80cf195
ba5hir <bashir15-12342@diu.edu.bd>
2023-04-23 20:40:38 +0600

    null undefined

--- Day 4/day 4.js
+++ Day 4/day 4.js
@@ -12,3 +12,26 @@ console.log(34 - 23);
 console.log(3 * 5);
 console.log(100 / 23);
 console.log(typeof 34);
+//modulus (vagshes)
+console.log(10 % 2);
+console.log(20 % 7);
+
+//exponential operation (power hisebe kah kore)
+console.log(3 ** 5); //3 ^ 5= 3 * 3 * 3 * 3 * 3 = 243
+console.log(2 ** 3); //2 ^ 3= 2 * 2 * 2 = 8
+
+//boolean
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof false);
+console.log(typeof true);
+
+//undefined
+let hello;
+console.log(hello);
+console.log(typeof hello);
+
+//null
+let hello1 = null;
+console.log(hello1);
+console.log(typeof hello1); //it's a javaScript bug
