ae118e95673c9e9bc482016aee91dabdd17535c4
ba5hir <bashir15-12342@diu.edu.bd>
2023-04-23 20:39:45 +0600

    data types

---
+++ Day 4/day 4.js
@@ -0,0 +1,14 @@
+//Primitive data types
+
+//string
+console.log("hello world");
+console.log(typeof "my mane is bashir");
+console.log(typeof "hello world");
+
+//working with number
+console.log(26);
+console.log(10 + 23);
+console.log(34 - 23);
+console.log(3 * 5);
+console.log(100 / 23);
+console.log(typeof 34);
